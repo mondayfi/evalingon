240e489d9c37627d14971fc7d2dcaf903653f58d
Samuel Klinkmann <samuel.klinkmann@sc5.io>
2015-06-10 23:18:42 +0300

    More content

--- js/app.js
+++ js/app.js
@@ -1,1 +1,108 @@
-window.addEventListener("load",function(){function e(e){var t=document.createElement("style");t.type="text/css",t.styleSheet?t.styleSheet.cssText=e:t.appendChild(document.createTextNode(e)),document.body.appendChild(t)}function t(){var e=document.createElement("div");e.className="mo-button",document.body.appendChild(e);var t=window.getComputedStyle(e),n="absolute"==t.position;return document.body.removeChild(e),n}function n(e,t){var n=t.target,o=n.classList;if(!o.contains("mo-button"))return!1;var i=n.getAttribute("data-event");if(i&&i!=e)return!1;n.setAttribute("data-event",e);var d,r=n.getBoundingClientRect(),s=t.offsetX;void 0!==s?d=t.offsetY:(s=t.clientX-r.left,d=t.clientY-r.top);var a=document.createElement("div");a.className="ripple",a.style.transform="translate("+s+"px, "+d+"px)",a.style.webkitTransform=a.style.transform,o.add("active"),n.appendChild(a),window.setTimeout(function(){var e;e=r.width==r.height?1.412*r.width:Math.sqrt(r.width*r.width+r.height*r.height);var t=2*e+"px",n=-e+"px";a.style.width=t,a.style.height=t,a.style.marginLeft=n,a.style.marginTop=n,a.classList.add("held")},20);var u="mousedown"==e?"mouseup":"touchend",l=function(e){document.removeEventListener(u,l),a.classList.add("done"),window.setTimeout(function(){n.removeChild(a),n.children.length||(o.remove("active"),n.removeAttribute("data-event"))},650)};document.addEventListener(u,l)}if(!t()){var o='.mo-button.fill::after{position:absolute;top:0;left:0;right:0;bottom:0;content:""}.mo-button.fill.active{border-radius:1000000px}.mo-button .ripple{position:absolute;border-radius:100%;background:currentColor;opacity:.2;transition:all .4s ease-out;width:0;height:0;pointer-events:none;-webkit-touch-callout:none;-webkit-user-select:none;-khtml-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none}.mo-button .ripple.held{opacity:.4}.mo-button .ripple.done{opacity:0}';e(o)}document.addEventListener("mousedown",function(e){0==e.button&&n(e.type,e)}),document.addEventListener("touchstart",function(e){for(var t=0;t<e.changedTouches.length;++t)n(e.type,e.changedTouches[t])})});
+
+window.addEventListener('load', function() {
+  function applyStyle(css) {
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    if (style.styleSheet) {
+      style.styleSheet.cssText = css;
+    } else {
+      style.appendChild(document.createTextNode(css));
+    }
+    document.body.appendChild(style);
+  }
+  function hasCSS() {
+    var test = document.createElement('div');
+    test.className = 'mo-button';
+    document.body.appendChild(test);
+    var s = window.getComputedStyle(test);
+    var result = s.position == 'absolute';
+    document.body.removeChild(test);
+    return result;
+  }
+  if (!hasCSS()) {
+    var css = '.mo-button.fill::after{position:absolute;top:0;left:0;right:0;bottom:0;content:""}.mo-button.fill.active{border-radius:1000000px}.mo-button .ripple{position:absolute;border-radius:100%;background:currentColor;opacity:.2;transition:all .4s ease-out;width:0;height:0;pointer-events:none;-webkit-touch-callout:none;-webkit-user-select:none;-khtml-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none}.mo-button .ripple.held{opacity:.4}.mo-button .ripple.done{opacity:0}';
+    applyStyle(css);
+  }
+
+  function startRipple(type, at) {
+    var holder = at.target;
+    var cl = holder.classList;
+    if (!cl.contains('mo-button')) {
+      return false;  // ignore
+    }
+
+    // Store the event use to generate this ripple on the holder: don't allow
+    // further events of different types until we're done. Prevents double-
+    // ripples from mousedown/touchstart.
+    var prev = holder.getAttribute('data-event');
+    if (prev && prev != type) {
+      return false;
+    }
+    holder.setAttribute('data-event', type);
+
+    // Create and position the ripple.
+    var rect = holder.getBoundingClientRect();
+    var x = at.offsetX;
+    var y;
+    if (x !== undefined) {
+      y = at.offsetY;
+    } else {
+      x = at.clientX - rect.left;
+      y = at.clientY - rect.top;
+    }
+    var ripple = document.createElement('div');
+    ripple.className = 'ripple';
+    ripple.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+    ripple.style.webkitTransform = ripple.style.transform;
+
+    // Activate/add the element, forcing an animation (setTimeout).
+    cl.add('active');
+    holder.appendChild(ripple);
+    window.setTimeout(function() {
+      var max;
+      if (rect.width == rect.height) {
+        max = rect.width * 1.412;
+      } else {
+        max = Math.sqrt(rect.width * rect.width + rect.height * rect.height);
+      }
+
+      var dim = max*2 + 'px';
+      var margin = -max + 'px';
+      ripple.style.width = dim;
+      ripple.style.height = dim;
+      ripple.style.marginLeft = margin;
+      ripple.style.marginTop = margin;
+      ripple.classList.add('held');
+    }, 20);
+
+    var releaseEvent = (type == 'mousedown' ? 'mouseup' : 'touchend');
+    var release = function(ev) {
+      // TODO: We don't check for _our_ touch here. Releasing one finger
+      // releases all ripples.
+      document.removeEventListener(releaseEvent, release);
+      ripple.classList.add('done');
+
+      // larger than animation: duration in css
+      window.setTimeout(function() {
+        holder.removeChild(ripple);
+        if (!holder.children.length) {
+          cl.remove('active');
+          holder.removeAttribute('data-event');
+        }
+      }, 650);
+    };
+    document.addEventListener(releaseEvent, release);
+  }
+
+  document.addEventListener('mousedown', function(ev) {
+    if (ev.button == 0) {
+      // trigger on left click only
+      startRipple(ev.type, ev);
+    }
+  });
+  document.addEventListener('touchstart', function(ev) {
+    for (var i = 0; i < ev.changedTouches.length; ++i) {
+      startRipple(ev.type, ev.changedTouches[i]);
+    }
+  });
+});

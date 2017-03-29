d0f34edd6d72b23b68c23cccaca0f6bd984436f9
Samuel Klinkmann <samuel.klinkmann@sc5.io>
2017-03-29 23:25:17 +0300

    Fix

--- evalingon-theme/header.php
+++ evalingon-theme/header.php
@@ -20,8 +20,8 @@
     <meta property="twitter:description" content="Eva Lingon Ltd. Oy Ab is a production company founded by Tage Rönnqvist and Rasmus Tåg in 2011." />
     <meta property="og:site_name" content="Evalingon.com" />
     <meta name="twitter:site" content="Evalingon.com" />
-    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/images/logo-share.jpg" />
-    <meta property="twitter:image" content="<?php echo get_template_directory_uri(); ?>/images/logo-share.jpg" />
+    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/images/logoshare.jpg" />
+    <meta property="twitter:image" content="<?php echo get_template_directory_uri(); ?>/images/logoshare.jpg" />
     <meta name="google-site-verification" content="bABtw1qMbsLWpp2uo8koJncu_rwqBjiRYkJNDs31Jxs" />
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,700,900" rel="stylesheet">
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
